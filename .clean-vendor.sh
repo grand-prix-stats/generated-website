f7b33c5b79fa6daeb84b4f7a4044954243a5bb4a
Eneko Alonso <eneko.alonso@gmail.com>
2017-07-09 21:25:52 -0700

    Season dashboard and driver dashboards

--- .clean-vendor.sh
+++ .clean-vendor.sh
@@ -2,7 +2,10 @@
 
 find vendors -name "*.html" -delete
 find vendors -name "*.md" -delete
+find vendors -name "*.markdown" -delete
+find vendors -name "*.yml" -delete
 find vendors -name bower.json -delete
+find vendors -name .bower.json -delete
 find vendors -name package.js -delete
 find vendors -name package.json -delete
 find vendors -name LICENSE -delete
@@ -10,4 +13,10 @@ find vendors -name License.txt -delete
 find vendors -name README -delete
 find vendors -name examples -exec rm -rf {} \;
 find vendors -name docs -exec rm -rf {} \;
+find vendors -name documentation -exec rm -rf {} \;
+find vendors -name lib -exec rm -rf {} \;
+find vendors -name vendor -exec rm -rf {} \;
+find vendors -name test -exec rm -rf {} \;
+find vendors -name tests -exec rm -rf {} \;
+find vendors -name spec -exec rm -rf {} \;
 
